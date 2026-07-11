2d25aa9989468cb638e69c6fca992fb9a3b54126
taeho2344 <taeho2344@gmail.com>
2026-07-11 19:46:31 +0900

    main.py correction

--- raspberry_pi_codes/main_plan.py
+++ raspberry_pi_codes/main_plan.py
@@ -1,45 +1,19 @@
-Alg main{
-
-
-
-    System Init
-
-
-
-    while(true){
-
-
-
-        if(Start Button){
-
-            Session();
-
-        }
-
-
-
-    } 
-
-
-
-}
-
-Alg Session{
-
-    while(!End Button){
-
-        Wait(5 min)
-
+def System_Init():
+def Session():
+    while !End Button:
+        Wait(5 min)//내부에서 wait 함수 쓰는거보단 시작 시간 받아서 시간차로 계산하는게 정확
         Capture Image
-
         Image Preprocessing
-
         CSE Analysis
-
         Save Result
-
         Control Desk
 
-    }
-
 }
+def main:
+
+    System_Init()
+    while 1:
+        if Start_Button:
+            Session()
+        analysis()
+        announcement()
