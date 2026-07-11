ef12762ea40e33fe0747e6dd7b80cc230fd90138
taeho2344 <taeho2344@gmail.com>
2026-07-11 19:47:56 +0900

    .

--- raspberry_pi_codes/main_plan.py
+++ raspberry_pi_codes/main_plan.py
@@ -2,11 +2,11 @@ def System_Init():
 def Session():
     while !End Button:
         Wait(5 min)//내부에서 wait 함수 쓰는거보단 시작 시간 받아서 시간차로 계산하는게 정확
-        Capture Image
-        Image Preprocessing
-        CSE Analysis
-        Save Result
-        Control Desk
+        Capture Image()
+        Image Preprocessing()
+        CSE Analysis()
+        Save Result()
+        Control Desk()
 
 }
 def main:
