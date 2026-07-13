a2531b7c5183ceeb53c9ccbab2da3bce456d600b
hhuuhh <hyj400806@gmail.com>
2026-07-13 12:51:31 +0900

    Update camera2.py

--- raspberry_pi_codes/camera2.py
+++ raspberry_pi_codes/camera2.py
@@ -1,11 +1,1 @@
-import cv2
-
-cap = cv2.VideoCapture(0)
-
-print("Opened:", cap.isOpened())
-
-ret, frame = cap.read()
-
-print("Ret:", ret)
-
-cap.release()
+python3 -c "from picamera2 import Picamera2; print('OK')"
