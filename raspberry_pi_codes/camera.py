82e0caeff04c2ab55206a71d23603d472265c939
taeho2344 <taeho2344@gmail.com>
2026-07-13 12:23:41 +0900

    videocapture check

--- raspberry_pi_codes/camera.py
+++ raspberry_pi_codes/camera.py
@@ -1,4 +1,6 @@
-for i in range(5):
-    cap = cv2.VideoCapture(i)
-    print(i, cap.isOpened())
-    cap.release()
+import cv2
+
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+print("Opened:", cap.isOpened())
+ret, frame = cap.read()
+print("Read:", ret)
